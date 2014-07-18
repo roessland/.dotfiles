03b5e3a8c65020f22b71273ed04caeaa8ba46c68
Andreas Røssland <andreas.rossland@silabs.com>
2014-07-18 10:11:28 +0200

    added wrong bash prompt

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -1,111 +1,122 @@
-# prompt examples:
-#   [3 jobs master virtualenv] ~/code/myproject/foo
-#   [1 job my-branch virtualenv] ~/code/bar/
-#   [virtualenv] ~/code/
-#   ~
-# Very, very fast, only requiring a couple of fork()s (and no forking at all to determine the current git branch)
-
-if [[ "$USER" == "root" ]]
-then
-    export PS1="\e[1;31m\]\u \[\e[1;33m\]\w\[\e[0m\] ";
-else
-    export PS1="\[\e[1;33m\]\w\[\e[0m\] ";
-fi
-
-# 100% pure Bash (no forking) function to determine the name of the current git branch
-gitbranch() {
-    export GITBRANCH=""
-
-    local repo="${_GITBRANCH_LAST_REPO-}"
-    local gitdir=""
-    [[ ! -z "$repo" ]] && gitdir="$repo/.git"
-
-    # If we don't have a last seen git repo, or we are in a different directory
-    if [[ -z "$repo" || "$PWD" != "$repo"* || ! -e "$gitdir" ]]; then
-        local cur="$PWD"
-        while [[ ! -z "$cur" ]]; do
-            if [[ -e "$cur/.git" ]]; then
-                repo="$cur"
-                gitdir="$cur/.git"
-                break
-            fi
-            cur="${cur%/*}"
-        done
-    fi
-
-    if [[ -z "$gitdir" ]]; then
-        unset _GITBRANCH_LAST_REPO
-        return 0
-    fi
-    export _GITBRANCH_LAST_REPO="${repo}"
-    local head=""
-    local branch=""
-    read head < "$gitdir/HEAD"
-    case "$head" in
-        ref:*)
-            branch="${head##*/}"
-            ;;
-        "")
-            branch=""
-            ;;
-        *)
-            branch="d:${head:0:7}"
-            ;;
-    esac
-    if [[ -z "$branch" ]]; then
-        return 0
-    fi
-    export GITBRANCH="$branch"
+#!/bin/bash
+#
+# DESCRIPTION:
+#
+#   Set the bash prompt according to:
+#    * the active virtualenv
+#    * the branch/status of the current git repository
+#    * the return value of the previous command
+#    * the fact you just came from Windows and are used to having newlines in
+#      your prompts.
+#
+# USAGE:
+#
+#   1. Save this file as ~/.bash_prompt
+#   2. Add the following line to the end of your ~/.bashrc or ~/.bash_profile:
+#        . ~/.bash_prompt
+#
+# LINEAGE:
+#
+#   Based on work by woods
+#
+#   https://gist.github.com/31967
+ 
+# The various escape codes that we can use to color our prompt.
+        RED="\[\033[0;31m\]"
+     YELLOW="\[\033[1;33m\]"
+      GREEN="\[\033[0;32m\]"
+       BLUE="\[\033[1;34m\]"
+  LIGHT_RED="\[\033[1;31m\]"
+LIGHT_GREEN="\[\033[1;32m\]"
+      WHITE="\[\033[1;37m\]"
+ LIGHT_GRAY="\[\033[0;37m\]"
+ COLOR_NONE="\[\e[0m\]"
+ 
+# Detect whether the current directory is a git repository.
+function is_git_repository {
+  git branch > /dev/null 2>&1
 }
-
-PS1_green='\[\e[32m\]'
-PS1_blue='\[\e[34m\]'
-PS1_reset='\[\e[0m\]'
-_mk_prompt() {
-    # Change the window title of X terminals 
-    case $TERM in
-        xterm*|rxvt*|Eterm)
-            echo -ne "\033]0;${USER}@${HOSTNAME%%.*}:${PWD/$HOME/~}\007"
-           ;;
-        screen)
-            echo -ne "\033_${USER}@${HOSTNAME%%.*}:${PWD/$HOME/~}\033\\"
-          ;;
-    esac
-
-    # Un-screw virtualenv stuff
-    if [[ ! -z "${_OLD_VIRTUAL_PS1-}" ]]; then
-        export PS1="$_OLD_VIRTUAL_PS1"
-        unset _OLD_VIRTUAL_PS1
+ 
+# Determine the branch/state information for this git repository.
+function set_git_branch {
+  # Capture the output of the "git status" command.
+  git_status="$(git status 2> /dev/null)"
+ 
+  # Set color based on clean/staged/dirty.
+  if [[ ${git_status} =~ "working directory clean" ]]; then
+    state="${GREEN}"
+  elif [[ ${git_status} =~ "Changes to be committed" ]]; then
+    state="${YELLOW}"
+  else
+    state="${LIGHT_RED}"
+  fi
+ 
+  # Set arrow icon based on status against remote.
+  remote_pattern="# Your branch is (.*) of"
+  if [[ ${git_status} =~ ${remote_pattern} ]]; then
+    if [[ ${BASH_REMATCH[1]} == "ahead" ]]; then
+      remote="↑"
+    else
+      remote="↓"
     fi
-
-    if [[ -z "${_MK_PROMPT_ORIG_PS1-}" ]]; then
-        export _MK_PROMPT_ORIG_PS1="$PS1"
-    fi
-
-    local prefix=()
-    local jobcount="$(jobs -p | wc -l)"
-    if [[ "$jobcount" -gt 0 ]]; then
-        local job="${jobcount##* } job"
-        [[ "$jobcount" -gt 1 ]] && job="${job}s"
-        prefix+=("$job")
-    fi
-
-    gitbranch
-    if [[ ! -z "$GITBRANCH" ]]; then
-        prefix+=("${PS1_green}$GITBRANCH${PS1_reset}")
-    fi
-
-    local virtualenv="${VIRTUAL_ENV##*/}"
-    if [[ ! -z "$virtualenv" ]]; then
-        prefix+=("${PS1_blue}$virtualenv${PS1_reset}")
-    fi
-
-    PS1="$_MK_PROMPT_ORIG_PS1"
-    if [[ ! -z "$prefix" ]]; then
-        PS1="[${prefix[@]}] $PS1"
-    fi
-
-    export PS1
+  else
+    remote=""
+  fi
+  diverge_pattern="# Your branch and (.*) have diverged"
+  if [[ ${git_status} =~ ${diverge_pattern} ]]; then
+    remote="↕"
+  fi
+ 
+  # Get the name of the branch.
+  branch_pattern="^# On branch ([^${IFS}]*)"
+  if [[ ${git_status} =~ ${branch_pattern} ]]; then
+    branch=${BASH_REMATCH[1]}
+  fi
+ 
+  # Set the final branch string.
+  BRANCH="${state}(${branch})${remote}${COLOR_NONE} "
 }
-export PROMPT_COMMAND=_mk_prompt
-
+ 
+# Return the prompt symbol to use, colorized based on the return value of the
+# previous command.
+function set_prompt_symbol () {
+  if test $1 -eq 0 ; then
+      PROMPT_SYMBOL="\$"
+  else
+      PROMPT_SYMBOL="${LIGHT_RED}\$${COLOR_NONE}"
+  fi
+}
+ 
+# Determine active Python virtualenv details.
+function set_virtualenv () {
+  if test -z "$VIRTUAL_ENV" ; then
+      PYTHON_VIRTUALENV=""
+  else
+      PYTHON_VIRTUALENV="${BLUE}[`basename \"$VIRTUAL_ENV\"`]${COLOR_NONE} "
+  fi
+}
+ 
+# Set the full bash prompt.
+function set_bash_prompt () {
+  # Set the PROMPT_SYMBOL variable. We do this first so we don't lose the
+  # return value of the last command.
+  set_prompt_symbol $?
+ 
+  # Set the PYTHON_VIRTUALENV variable.
+  set_virtualenv
+ 
+  # Set the BRANCH variable.
+  if is_git_repository ; then
+    set_git_branch
+  else
+    BRANCH=''
+  fi
+ 
+  # Set the bash prompt variable.
+  PS1="
+${PYTHON_VIRTUALENV}${GREEN}\H ${YELLOW}\w${COLOR_NONE} ${BRANCH}
+${PROMPT_SYMBOL} "
+}
+ 
+# Tell bash to execute this function just before displaying its prompt.
+PROMPT_COMMAND=set_bash_prompt
