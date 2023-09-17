ec8eb86c75a324952a403bf0a56411623de2441a
Andreas Røssland <aros@hey.com>
2023-09-17 14:00:19 +0000

    fix broken symlinks

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -2,11 +2,11 @@
 # running it multiple times is safe.
 
 # Same for every machine
-ln -sn $HOME/dotfiles/vimrc $HOME/.vimrc
-ln -sn $HOME/dotfiles/vim/ $HOME/vim
-ln -sn $HOME/dotfiles/tmux.conf $HOME/tmux.conf
-ln -sn $HOME/dotfiles/gitconfig $HOME/gitconfig
-ln -sn $HOME/dotfiles/taskrc $HOME/taskrc
+ln -sn $HOME/.dotfiles/vimrc $HOME/.vimrc
+ln -sn $HOME/.dotfiles/vim/ $HOME/.vim
+ln -sn $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
+ln -sn $HOME/.dotfiles/gitconfig $HOME/.gitconfig
+ln -sn $HOME/.dotfiles/taskrc $HOME/.taskrc
 git clone http://github.com/gmarik/Vundle.vim.git $HOME/vim/bundle/Vundle.vim
 vim +BundleInstall +qall
 echo "Fatal Vundle error can be ignored :)"
