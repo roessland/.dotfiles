929bffd5328f4ed6d012ee5025b8e5e7b91d7c80
Andreas Røssland <andreas@enode.io>
2023-10-17 09:54:22 +0200

    nvim: try inlay hints

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { "neovim/nvim-lspconfig", opts = { inlay_hints = { enabled = true } } },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
