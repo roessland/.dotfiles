d0a3fe21169f38efa2ce830359388bee2259f43d
Andreas Røssland <aros@hey.com>
2023-10-17 09:41:59 +0200

    symlink nvim properly

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  opts = {
+    filetypes = {
+      lua = true,
+      yaml = true,
+      go = true,
+      rust = true,
+      julia = true,
+      python = true,
+    },
+  },
+}
--- setup_macos.sh
+++ setup_macos.sh
@@ -2,15 +2,15 @@
 # running it multiple times is safe.
 
 # Same for every machine
-ln -s ~/.dotfiles/vimrc ~/.vimrc
-ln -s ~/.dotfiles/vim ~/.vim
-ln -s ~/.dotfiles/nvim ~/.config/
-ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/ideavimrc ~/.ideavimrc
+ln -sn ~/.dotfiles/vimrc ~/.vimrc
+ln -sn ~/.dotfiles/vim ~/.vim
+ln -sn ~/.dotfiles/nvim ~/.config/
+ln -sn ~/.dotfiles/gitconfig ~/.gitconfig
+ln -sn ~/.dotfiles/ideavimrc ~/.ideavimrc
 mkdir -p ~/.config/fd/
-ln -s ~/.dotfiles/ignore ~/.config/fd/ignore # for fd
-ln -s ~/.dotfiles/ignore ~/.ignore # for fd, rg, ag
-ln -s ~/.dotfiles/ignore ~/.fdignore # for fd
+ln -sn ~/.dotfiles/ignore ~/.config/fd/ignore # for fd
+ln -sn ~/.dotfiles/ignore ~/.ignore # for fd, rg, ag
+ln -sn ~/.dotfiles/ignore ~/.fdignore # for fd
 #ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 git clone http://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Error from Vundle can be ignored"
